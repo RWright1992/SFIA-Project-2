934704cee0db7ae82696b9dd4a67186144094d9a
RWright1992 <ryanwright1992@gmail.com>
2020-08-21 16:38:02 +0000

    Adding Basic functionality for services to link

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,5 +1,5 @@
 #importin render_template redirect and url_for from flask module
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, jsonify
 #importing app object from __init__.py
 from application import app
 #importing requests
@@ -12,6 +12,25 @@ def home():
 
 #route for generating random username + game
 @app.route('/username/game', methods = ['GET'])
+def namegame():
 	username = requests.get('http://service2:5001/user/randomname')
 	game = requests.get('http://service3:5002/user/randomgame')
-	return render_template('namegame.html', title='Username and Game', username=username.txt, game=game.txt)
+	return render_template('namegame.html', title='Username and Game', username=username.text, game=game.text)
+
+'''
+@app.route('/username/prediction', methods = ['GET'])
+def prediction():
+	prediction = requests.get('http://service4:5003/user/8ball')
+'''
+
+#route for generating random username + game
+@app.route('/username/game1', methods = ['GET', 'POST'])
+def prediction():
+	username = requests.get('http://service2:5001/user/randomname')
+	game = requests.get('http://service3:5002/user/randomgame')
+	username_text = username.text
+	game_text = game.text
+	middle = ','
+	username_game = username_text + middle + game_text
+	gamename_post = requests.post('http://service4:5003/user/8ball', data=username_game)
+	return render_template('prediction.html', title="Should you play", prediction=gamename_post.text, username=username.text, game=game.text)
--- service3/application/routes.py
+++ service3/application/routes.py
@@ -10,6 +10,6 @@ from random_username.generate import generate_username
 #creating route which will generate random switch online game
 @app.route('/user/randomgame', methods = ['GET'])
 def user_name1():
-	games = ['Mario Kart','Monster Hunter Generations','Tetris 99','Mario Party', 'Splatoon 2', 'Super Smash Bros', 'Arms', 'Doom', 'Rocket League', 'Dragonball Fighter Z' ]
-	return Response(games[randint(0,9)], mimetype='text/plain')
+	games = ['Mario Kart','Monster Hunter Generations','Tetris 99' ]
+	return Response(games[randint(0,2)], mimetype='text/plain')
 
